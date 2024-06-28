7679a0d767885f9d587ae30d18666866853bc67b
wanghaisheng <edwin_uestc@163.com>
2024-06-28 20:29:25 +0800

    fix

--- check_index_date.py
+++ check_index_date.py
@@ -8,11 +8,12 @@ import shutil
 import zipfile
 from bs4 import BeautifulSoup
 from DataRecorder import Recorder
+from datetime import datetime
+import asyncio
+from loguru import logger
+from domainindexdate import process_domains
 
 
-filename = os.getenv("URL")
-# filename = "100"
-counts = os.getenv("counts")
 
 # 创建一个新的列"store url"，并初始化为None
 
@@ -126,136 +127,6 @@ def save_valid_proxies(config_file, valid_proxies):
             print(f"Error loading or saving proxies: {e}")
 
 
-def get_domain_first_index_date1(row, valid_proxies, workingproxies):
-    sellerid = row["sellerId"].strip()
-    from urllib.parse import urlencode
-
-    domain = "https://www.amazon.com/s?me=" + sellerid.strip()
-    # domain = (
-    # f"https://www.amazon.com/s?me={sellerid.strip()}&marketplaceID=ATVPDKIKX0DER"
-    # )
-    # domain = f"https://www.amazon.com/sp?ie=UTF8&seller={sellerid.strip()}"
-
-    url = f"https://www.google.com/search?q=About+{domain}&tbm=ilp&sa=X&ved=2ahUKEwj3jraUsoGGAxUvSGwGHUbfAEwQv5AHegQIABAE"
-
-    print("url to", url)
-    # Source
-    # www.remini.com was first indexed by Google in July 2021
-
-    max_retries = 5
-
-    proxies = None
-    pro_str = None
-    isbanned = True
-    for i in range(max_retries):
-        print(f"{i}th=={domain}")
-        if i == 0:
-            # proxies = {
-            #     "http": "socks5h://127.0.0.1:1080",
-            #     "https": "socks5h://127.0.0.1:1080",
-            # }
-            pass
-        elif workingproxies != []:
-
-            workingproxies = [p for p in workingproxies if p is not None]
-            pro_str = random.choice(workingproxies)
-            if "sock" not in pro_str:
-
-                proxy = "http://{}".format(pro_str)
-                proxies = {"http": proxy, "https": proxy.replace("http", "https")}
-
-            else:
-                proxy = pro_str
-                # if 'socks4' in proxy:
-                #     proxy=proxy.replace('socks4','socks4h')
-                # if 'socks5' in proxy:
-
-                #     proxy=proxy.replace('socks5','socks5h')
-
-                # proxy = f"http://{rand_proxies()}"
-                proxies = {"http": proxy, "https": proxy}
-            # print(f"{i}========load exist proxy ======= {proxies}")
-        elif valid_proxies == []:
-            pro_str = get_proxy()["proxy"]
-
-            proxy = "http://{}".format(pro_str)
-
-            # proxy = f"http://{rand_proxies()}"
-            proxies = {"http": proxy, "https": proxy.replace("http", "https")}
-        else:
-
-            pro_str = random.choice(valid_proxies)
-            proxies = {"http": pro_str, "https": pro_str}
-
-            print(f"{i}========load exist proxy ======= {proxies}")
-
-        print(f"use google to check {domain} store index date")
-        try:
-            r = requests.get(url, headers=random.choice(headers_list), proxies=proxies)
-            print("status code")
-            if r.status_code == 200:
-                print("okk")
-            else:
-                print("3333333333")
-            html_doc = r.content
-            # print("==============get index date", r.content)
-            # Parse the HTML document with BeautifulSoup
-            soup = BeautifulSoup(html_doc, "html.parser")
-
-            # Find all elements that contain the text 'aaa'
-
-            elements_with_aaa = soup.find_all(
-                lambda tag: "Site first indexed by Google" in tag.get_text()
-            )
-
-            # # Output the text content of each element that contains 'aaa'
-            # for element in elements_with_aaa:
-            # print(element.get_text().strip())
-            if len(elements_with_aaa) > 0:
-                r = elements_with_aaa[0].get_text()
-                if "Site first indexed by Google" in r:
-                    r = r.split("Site first indexed by Google")
-                    # print("get data", r[0])
-                    print("get data", r[-1])
-
-                    save_valid_proxies(
-                        folder_path + "/proxies_config.txt", workingproxies
-                    )
-                    # save_valid_proxies(
-                    #     folder_path + "/store-indexdate.txt", [sellerid + "-", r[-1]]
-                    # )
-                    dr.add_data({"sellerid": sellerid, "date": r[-1]})  # 接收dict数据
-
-                    # return r[-1]
-                    break
-                else:
-                    continue
-
-            else:
-                save_valid_proxies(
-                    folder_path + "/failed-store-indexdate.txt", [domain]
-                )
-                break
-
-        except Exception as e:  # 捕获请求相关的异常
-            print(f"Attempt failed with{pro_str} error: {e}")
-
-            # print(f"Attempt {i+1} failed with error:{e}")
-            if pro_str in workingproxies:
-                workingproxies.remove(pro_str)
-                save_valid_proxies("proxies_config.txt", workingproxies)
-
-            if i < max_retries - 1:
-                # time.sleep(2**i)  # 指数退避策略
-                # print("Retrying...")
-                continue  # 继续下一次重试
-            else:
-                print("Max retries reached. Exiting without a successful response.")
-                save_valid_proxies("failedstore.txt", [domain])
-
-                break  # 最大重试次数达到，退出循环
-
-
 def zip_folder(
     folder_path, output_folder, max_size_mb, zip_file, zip_temp_file, zip_count
 ):
@@ -307,57 +178,6 @@ def zip_folder(
     print(f"Created '{final_zip_path}' (size: {os.path.getsize(final_zip_path)} bytes)")
 
 
-# Example usage:
-# zip_folder('/path/to/folder', '/path/to/output', 100, None, None, 0)
-def zip_folder_old(
-    folder_path, output_folder, max_size_mb, zip_file, zip_temp_file, zip_count
-):
-    # Create the output folder if it doesn't exist
-    os.makedirs(output_folder, exist_ok=True)
-
-    # Convert the maximum size from MB to bytes
-    max_size_bytes = max_size_mb * 1024 * 1024
-
-    # Iterate over the directory tree
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-
-            # Add each file to the current ZIP archive
-            zip_file.write(file_path)
-
-            # Check if the current ZIP file exceeds the maximum size
-            if os.stat(file_path).st_size > max_size_bytes:
-                # Close the current ZIP archive
-                zip_file.close()
-
-                # Move the current ZIP file to the output folder
-                shutil.move(
-                    zip_temp_file,
-                    os.path.join(output_folder, f"archive{zip_count}.zip"),
-                )
-
-                print(
-                    f"Created 'archive{zip_count}.zip' (size: {os.path.getsize(os.path.join(output_folder, f'archive{zip_count}.zip'))} bytes)"
-                )
-
-                # Create a new ZIP archive for the remaining files
-                zip_count += 1
-                zip_temp_file = os.path.join(output_folder, f"temp{zip_count}.zip")
-                zip_file = zipfile.ZipFile(zip_temp_file, "w", zipfile.ZIP_DEFLATED)
-
-                # Delete the original file after adding it to the ZIP archive
-                os.remove(file_path)
-
-    # Close the last ZIP archive
-    zip_file.close()
-
-    # Move the last ZIP file to the output folder
-    shutil.move(zip_temp_file, os.path.join(output_folder, f"archive{zip_count}.zip"))
-
-    print(
-        f"Created 'archive{zip_count}.zip' (size: {os.path.getsize(os.path.join(output_folder, f'archive{zip_count}.zip'))} bytes)"
-    )
 
 
 def test_proxy(proxy_url, test_url):
@@ -417,11 +237,6 @@ if valid_proxies:
     print("Cleaned Proxies:", cleaned_proxies)
 folder_path = "./result"
 
-# 读取Excel文件A和CSV文件B
-csv_b = pd.read_csv(filename + ".csv")
-if counts and int(counts) > 0:
-    csv_b = csv_b.head(int(counts))
-
 if os.path.exists(folder_path) == False:
     os.mkdir(folder_path)
 
@@ -435,40 +250,40 @@ if not os.path.exists(output_folder):
 else:
     # If the directory exists, do nothing (pass)
     print("Directory 'output' already exists.")
+filename = os.getenv("filename")
+colname = os.getenv("colname")
 
-dr = Recorder(path=output_folder + "/data.csv", cache_size=500)
+#
+#  filename = "100"
+counts = os.getenv("counts")
 
-with ThreadPoolExecutor(
-    max_workers=200
-) as executor:  # You can adjust the number of workers
-    future_to_domain = {
-        executor.submit(
-            get_domain_first_index_date1, row, valid_proxies, valid_proxies
-        ): row["sellerId"]
-        for index, row in csv_b.iterrows()
-    }
 
-    for future in as_completed(future_to_domain):
-        domain = future_to_domain[future]
-        try:
-            # Get the result of the execution, if you have returned something from process_row
-            result = future.result()
-            dr.record()  # 手动调用写入方法
 
-        except Exception as exc:
-            print(f"Generated an exception: {exc} for domain {domain}")
-        else:
-            # Process the result if needed
-            print(f"Successfully processed: {domain}")
+start=datetime.now()
+inputfilepath=filename + ".csv"
+domainkey='Domain'
+logger.add(f"domain-index-ai.log")
+# print(domains)
+outfilepath=inputfilepath.replace('.csv','-index.csv')
+outfile = Recorder(output_folder+'/'+outfilepath, cache_size=50)
+asyncio.run(process_domains(inputfilepath,colname,outfilepath,outfile))
+end=datetime.now()
+print('costing',end-start)
+outfile.record()
+
+
+
+
+
     # Specify the maximum size of each RAR file in MB
-    max_size_mb = 1500
+max_size_mb = 1500
 
-    # Create a temporary ZIP file for the first archive
-    zip_count = 1
-    zip_temp_file = os.path.join(folder_path, f"temp{zip_count}.zip")
-    zip_file = zipfile.ZipFile(zip_temp_file, "w", zipfile.ZIP_DEFLATED)
+# Create a temporary ZIP file for the first archive
+zip_count = 1
+zip_temp_file = os.path.join(folder_path, f"temp{zip_count}.zip")
+zip_file = zipfile.ZipFile(zip_temp_file, "w", zipfile.ZIP_DEFLATED)
 
-    # Compress the folder into multiple ZIP archives
-    zip_folder(
-        folder_path, output_folder, max_size_mb, zip_file, zip_temp_file, zip_count
-    )
+# Compress the folder into multiple ZIP archives
+zip_folder(
+    folder_path, output_folder, max_size_mb, zip_file, zip_temp_file, zip_count
+)

092b85a5187f818f0a962b5af2933c5d85f618fd
wanghaisheng <edwin_uestc@163.com>
2024-06-29 23:47:01 +0800

    fix 429 hangon

--- domainborndateRevved.py
+++ domainborndateRevved.py
@@ -228,17 +228,18 @@ async def lookup_domain(domain: str,proxy_url: str, semaphore: asyncio.Semaphore
                         new_domain = Domain(
                             url=domain,
                         bornat=creation_date_str)
-                        add_domain(new_domain)                        
+                        add_domain(new_domain)       
+                        return True                 
                 else:
                     print('status 200 but without results key',data)
+                    raise
 
 
 
-
-                return True
+            
             else:
                 logger.warning(f"Non-200 status code: {response.status} for {domain}")
-                return False
+                raise
 
         except asyncio.TimeoutError as e:
             logger.info(f'{RED} TimeoutError {GREY}| --- | {PURPLE}{query_url.ljust(50)} {GREY}| {CYAN}{domain} {RED}| {e}{RESET}')
